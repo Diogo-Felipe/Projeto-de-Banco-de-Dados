f55821b750f105092c80eb94d9ac7a38d6898539
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-15 12:50:47 -0300

    Segunda leva de inserts (referente a letra b do projeto)

---
+++ Obras/Inserts_b.sql
@@ -0,0 +1,37 @@
+-- 
+-- Inserts Pavimentação Rodovia PA-140 BD de Obras
+-- Ver. 1.0 em 15/02/2020
+-- Autor: Diogo L.C. Felipe
+--
+
+INSERT INTO Obras ( descricao ) VALUES ( 'Pavimentação Rodovia PA-140' );
+
+INSERT INTO Profissao ( nome ) VALUES ( 'Agrimessor' );
+
+INSERT INTO Profissional ( nome ) VALUES ( 'João Pimenta de Oliveira' );
+INSERT INTO Profissional ( nome ) VALUES ( 'Adamastor Capstrano' );
+INSERT INTO Profissional ( nome ) VALUES ( 'Rafael Cabeça de Castro' );
+
+INSERT INTO Trabalha ( 
+    datainicio,
+    datafim,
+    idobra,
+    idpessoal,
+    idprof
+) VALUES ('15/01/2017', NULL, 2, 8, 1 );
+
+INSERT INTO Trabalha ( 
+    datainicio,
+    datafim,
+    idobra,
+    idpessoal,
+    idprof
+) VALUES ('15/01/2017', NULL, 2, 9, 1 );
+
+INSERT INTO Trabalha ( 
+    datainicio,
+    datafim,
+    idobra,
+    idpessoal,
+    idprof
+) VALUES ('15/01/2017', '30/11/2018', 2, 10, 4 );
