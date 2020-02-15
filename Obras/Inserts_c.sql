ba03cb1740362a06091bd17e1edd044fec1d25f3
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-15 12:51:07 -0300

    Terceira leva de inserts (referente a letra c do projeto)

---
+++ Obras/Inserts_c.sql
@@ -0,0 +1,31 @@
+-- 
+-- Inserts Praça Antônio Carlos BD de Obras
+-- Ver. 1.0 em 15/02/2020
+-- Autor: Diogo L.C. Felipe
+--
+
+INSERT INTO Obras ( descricao ) VALUES ( 'Praça Antônio Carlos' );
+
+INSERT INTO Trabalha ( 
+    datainicio,
+    datafim,
+    idobra,
+    idpessoal,
+    idprof
+) VALUES ('13/05/2012', '26/09/2013', 3, 4, 2 );
+
+INSERT INTO Trabalha ( 
+    datainicio,
+    datafim,
+    idobra,
+    idpessoal,
+    idprof
+) VALUES ('13/05/2012', '26/09/2013', 3, 5, 2 );
+
+INSERT INTO Trabalha ( 
+    datainicio,
+    datafim,
+    idobra,
+    idpessoal,
+    idprof
+) VALUES ('13/05/2012', '26/09/2013', 3, 8, 1 );
