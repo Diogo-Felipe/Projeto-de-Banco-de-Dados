29651b571069060b52b7f6dadb26ac9b31f36914
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-15 12:51:28 -0300

    Primeira leva de selects (referente a letra a do projeto)

---
+++ Obras/Selects_a.sql
@@ -0,0 +1,10 @@
+-- 
+-- Selects BD de Obras
+-- Ver. 1.0 em 15/02/2020
+-- Autor: Diogo L.C. Felipe
+--
+
+SELECT Profissional.nome, Profissao.nome 
+FROM ((Trabalha 
+INNER JOIN Profissional ON Trabalha.idpessoa = Profissional.idpessoa)
+INNER JOIN Profissao ON Trabalha.idprof = Profissao.idprof);
