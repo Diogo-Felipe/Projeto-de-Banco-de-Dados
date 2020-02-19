be5595e368cdd97ba33c65968bc0ed8ab2f9a15d
Diogo Felipe <dlcfelipp@gmail.com>
2020-02-19 09:46:07 -0300

    Coreção do select de profissional

--- Obras/Selects_a.sql
+++ Obras/Selects_a.sql
@@ -1,10 +1,11 @@
 -- 
 -- Selects BD de Obras
--- Ver. 1.0 em 15/02/2020
+-- Ver. 1.1.0 em 15/02/2020
 -- Autor: Diogo L.C. Felipe
 --
 
-SELECT Profissional.nome, Profissao.nome 
+SELECT DISTINCT Profissional.nome, Profissao.nome 
 FROM ((Trabalha 
 INNER JOIN Profissional ON Trabalha.idpessoa = Profissional.idpessoa)
-INNER JOIN Profissao ON Trabalha.idprof = Profissao.idprof);
+INNER JOIN Profissao ON Trabalha.idprof = Profissao.idprof)
+ORDER BY Profissional.nome;
