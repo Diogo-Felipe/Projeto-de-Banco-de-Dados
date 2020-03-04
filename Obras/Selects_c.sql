575a756b630fb57bf529cc86e55b563609ac9868
Diogo Felipe <dlcfelipp@gmail.com>
2020-03-04 10:05:37 -0300

    Correção

--- Obras/Selects_c.sql
+++ Obras/Selects_c.sql
@@ -3,3 +3,8 @@
 -- Ver. 1.0.0 em 04/03/2020
 -- Autor: Diogo L.C. Felipe
 --
+
+SELECT Profissao.nome, COUNT(Profissao.nome) 
+from Profissao
+    INNER JOIN Trabalha ON Profissao.idprof = Trabalha.idprof
+GROUP BY (Profissao.nome);
