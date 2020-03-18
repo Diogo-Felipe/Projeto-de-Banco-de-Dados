55f6ae842e8f2212d0b349f4adb2b8cddbf89b68
Diogo Felipe <dlcfelipp@gmail.com>
2020-03-18 16:53:10 -0300

    Select b

--- Academia/Selects.sql
+++ Academia/Selects.sql
@@ -1,6 +1,17 @@
-SELECT Aluno.nome FROM ALuno 
+SELECT Aluno.nome FROM Aluno 
     INNER JOIN Matricula ON Matricula.matricula = Aluno.matricula
     INNER JOIN ItensMatricula ON ItensMatricula.idMatricula = Matricula.idMatricula
     INNER JOIN Modalidade ON Modalidade.idModalidade = ItensMatricula.idModalidade
 WHERE Modalidade.descricao LIKE '%atação%';
 
+SELECT Modalidade.descricao,
+    (
+        SELECT count(*) 
+        FROM Aluno a 
+            INNER JOIN Matricula ma ON ma.matricula = a.matricula
+            INNER JOIN ItensMatricula im ON im.idMatricula = ma.idMatricula
+            INNER JOIN Modalidade mo ON mo.idModalidade = im.idModalidade
+            WHERE Matricula.dataMatricula BETWEEN '01/01/2020' AND '21/12/2020'
+                AND mo.idModalidade = mop.idModalidade
+    ) Alunos
+FROM Modalidade mop;
