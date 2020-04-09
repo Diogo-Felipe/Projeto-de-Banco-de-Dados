1b61d4e3d4032b908c32595bf89b601d966a22db
Diogo Felipe <dlcfelipp@gmail.com>
2020-04-09 16:25:05 -0300

    Questão 2

--- Academia/Exerc_avaliativo.sql
+++ Academia/Exerc_avaliativo.sql
@@ -1,1 +1,7 @@
-SELECT acad.razaosocial, (SELECT count(*) FROM academia ac INNER JOIN aluno alu ON ac.cnpj = alu.cnpj INNER JOIN matricula mat ON alu.matricula = mat.matricula WHERE acad.cnpj = ac.cnpj and mat.situacao = 'A') alunos_ativos, (SELECT count(*) FROM academia ac INNER JOIN aluno alu ON ac.cnpj = alu.cnpj INNER JOIN matricula mat ON alu.matricula = mat.matricula WHERE acad.cnpj = ac.cnpj and mat.situacao = 'I') alunos_inativos FROM academia acad
+-- 1 A
+SELECT acad.razaosocial, alu.nome FROM academia acad INNER JOIN aluno alu ON acad.cnpj = alu.cnpj ORDER BY acad.razaosocial, alu.nome
+
+-- 2 C
+SELECT count(*) FROM academia acad INNER JOIN aluno alu ON acad.cnpj = alu.cnpj INNER JOIN matricula mat ON alu.matricula = mat.matricula INNER JOIN itensmatricula item ON mat.idmatricula = item.idmatricula INNER JOIN modalidade moda ON item.idmodalidade = moda.idmodalidade WHERE acad.cnpj = '1111111111111' AND upper(moda.descricao) like '%MUSCULA%'
+
+
