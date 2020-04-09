f919361206fb55a99b45781d9be417ea7cfcd4c9
Diogo Felipe <dlcfelipp@gmail.com>
2020-04-09 17:01:46 -0300

    Questão 5

--- Academia/Exerc_avaliativo.sql
+++ Academia/Exerc_avaliativo.sql
@@ -13,6 +13,27 @@ GO
 CREATE VIEW V_ATIVOS AS (
     SELECT acad.razaosocial, alu.nome, moda.descricao
     FROM academia acad
-    INNER JOIN aluno alu ON acad.cnpj = alu.cnpj INNER JOIN matricula mat ON alu.matricula = mat.matricula INNER JOIN itensmatricula item ON mat.idmatricula = item.idmatricula INNER JOIN modalidade moda ON item.idmodalidade = moda.idmodalidade
+        INNER JOIN aluno alu ON acad.cnpj = alu.cnpj INNER JOIN matricula mat ON alu.matricula = mat.matricula INNER JOIN itensmatricula item ON mat.idmatricula = item.idmatricula INNER JOIN modalidade moda ON item.idmodalidade = moda.idmodalidade
     WHERE mat.situacao = 'A'
 )
+
+GO
+
+-- 5
+
+SELECT acad.nome, moda.descricao
+FROM academia acad
+    INNER JOIN aluno alu ON acad.cnpj = alu.cnpj INNER JOIN matricula mat ON alu.matricula = mat.matricula INNER JOIN itensmatricula item ON mat.idmatricula = item.idmatricula INNER JOIN modalidade moda ON item.idmodalidade = moda.idmodalidade
+WHERE (
+    (SELECT count(*) FROM academia ac
+        INNER JOIN aluno alu ON ac.cnpj = alu.cnpj INNER JOIN matricula mat ON alu.matricula = mat.matricula INNER JOIN itensmatricula item ON mat.idmatricula = item.idmatricula INNER JOIN modalidade moda ON item.idmodalidade = moda.idmodalidade
+    WHERE mat.situacao = 'A'
+        and acad.cnpj = ac.cnpj)
+                >=
+    (
+        (SELECT count(*) FROM matricula)
+         			/
+        (SELECT count(*) FROM academia)
+    )
+)
+GROUP BY acad.nome, moda.descricao;
