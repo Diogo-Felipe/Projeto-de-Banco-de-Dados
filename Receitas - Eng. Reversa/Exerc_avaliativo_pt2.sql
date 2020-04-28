43ad2bdc59eea099a56f02367bec3d2821c51606
Diogo Felipe <dlcfelipp@gmail.com>
2020-04-28 11:23:37 -0300

    Q.4

--- Receitas - Eng. Reversa/Exerc_avaliativo_pt2.sql
+++ Receitas - Eng. Reversa/Exerc_avaliativo_pt2.sql
@@ -45,3 +45,51 @@ CREATE TABLE composicao (
     CONSTRAINT fk_receita FOREIGN KEY (idreceita) REFERENCES receita (idreceita),
     CONSTRAINT fk_ingredientes FOREIGN KEY (idingredientes) REFERENCES ingredientes (idingredientes)
 );
+
+-- 4 Questão
+
+INSERT INTO nivel (descricao) VALUES ('Alimentos Energéticos');
+INSERT INTO nivel (descricao) VALUES ('Alimentos Reguladores');
+INSERT INTO nivel (descricao) VALUES ('Alimentos Construtores');
+INSERT INTO nivel (descricao) VALUES ('Alimentos Energéticos Extra');
+
+INSERT INTO grupoalimentar (descricao, complemento, nivel)
+VALUES ('Carboidratos', 'Alimentos que fornecem energia, pois ao serem comsumidos...', 1);
+INSERT INTO grupoalimentar (descricao, complemento, nivel)
+VALUES ('Verduras e Legumes','Fonte de fibras, vitaminas e minerais', 2);
+INSERT INTO grupoalimentar (descricao, complemento, nivel)
+VALUES ('Futas', 'Fontes de fobras, vitaminas e minerais, aumenta o nível...', 2);
+INSERT INTO grupoalimentar (descricao, complemento, nivel)
+VALUES ('Leites e Derivados', 'Fonte de proteínas e de cálcio, que é essencial...', 3);
+INSERT INTO grupoalimentar (descricao, complemento, nivel)
+VALUES ('Canes e Ovos', 'Fonte de proteína de origem animal, são ricos em ferro...', 3);
+INSERT INTO grupoalimentar (descricao, complemento, nivel)
+VALUES ('Leguminosas e Olegiminosas', 'Fonte de protína vegeral. Também...', 3);
+INSERT INTO grupoalimentar (descricao, complemento, nivel)
+VALUES ('Óleos e Gorduras', 'Fonte de energia e são responsáveis...', 4);
+INSERT INTO grupoalimentar (descricao, complemento, nivel)
+VALUES ('Açucares e doces', 'São alimentos ricos em carboidratos...', 4);
+
+INSERT INTO receita (
+    nome,
+    instrucoes
+) VALUES (
+    'Bolo de Chocolate',
+    'Bata no liquidificador os ovos, o açúcar, o achocolatado, o óleo e a água; Coloque a mistura em um...'
+);
+
+INSERT INTO ingredientes (nome, unidade, grupo) VALUES ('OVO', 'un', 4);
+INSERT INTO ingredientes (nome, unidade, grupo) VALUES ('TRIGO', 'g', 1);
+INSERT INTO ingredientes (nome, unidade, grupo) VALUES ('AÇUCAR', 'g', 8);
+INSERT INTO ingredientes (nome, unidade, grupo) VALUES ('ACHOCOLATADO', 'g', 8);
+INSERT INTO ingredientes (nome, unidade, grupo) VALUES ('OLEO', 'ml', 7);
+INSERT INTO ingredientes (nome, unidade) VALUES ('AGUA', 'ml');
+INSERT INTO ingredientes (nome, unidade) VALUES ('FERMENTO', 'g');
+
+INSERT INTO composicao (qtd, idreceita, idingredientes) VALUES (3.00, 1, 1);
+INSERT INTO composicao (qtd, idreceita, idingredientes) VALUES (250.00, 1, 2);
+INSERT INTO composicao (qtd, idreceita, idingredientes) VALUES (200.00, 1, 3);
+INSERT INTO composicao (qtd, idreceita, idingredientes) VALUES (90.00, 1, 4);
+INSERT INTO composicao (qtd, idreceita, idingredientes) VALUES (180.00, 1, 5);
+INSERT INTO composicao (qtd, idreceita, idingredientes) VALUES (240.00, 1, 6);
+INSERT INTO composicao (qtd, idreceita, idingredientes) VALUES (10.00, 1, 7);
